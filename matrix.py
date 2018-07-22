13ee97f924187a0896d4a275827de1641bee6240
AnYuZhuang <40970101+AnYuZhuang@users.noreply.github.com>
2018-07-22 09:00:50 -0700

    Update matrix.py

--- matrix.py
+++ matrix.py
@@ -2,17 +2,20 @@ import random
 
 from copy import deepcopy
 
-
-class Matrix:
+class Matrix():
 
     def __init__(self, nrows, ncols):
         """Construct a (nrows X ncols) matrix"""
-        pass
+        self.nrows=nrows 
+        self.ncols=ncols
+        for i in range(int(self.nrows)):
+            for j in range(int(self.ncols)):
+                print(random.randint(0,9),end=' ') 
+            print("")
 
     def add(self, m):
         """return a new Matrix object after summation"""
-        pass
-
+        
     def sub(self, m):
         """return a new Matrix object after substraction"""
         pass
@@ -23,8 +26,11 @@ class Matrix:
 
     def transpose(self):
         """return a new Matrix object after transpose"""
-        pass
+        #print (list(map(list, zip(*self.matrix))))
     
     def display(self):
         """Display the content in the matrix"""
         pass
+
+nrows=Matrix(input("Enter A Matrix's rows:"),input("Enter A Matrix's cols:"))
+ncols=Matrix(input("Enter B Matrix's rows:"),input("Enter B Matrix's cols:"))
